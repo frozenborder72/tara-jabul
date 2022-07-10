f8acc672f064634c02d00d36d5bcb46deb57a1c7
Marco <mar.manunta@gmail.com>
2022-07-10 11:59:07 +0200

    fixed logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import logo from '../assets/images/LOGO/optimized/instagram.png'
+import logo from '../assets/images/LOGO.png'
 import image from '../assets/images/Digital Paintings/optimized/BICYCLE-GIRL.jpg'
 
 const Home = () => {
   return (
     <main>
-      <div className="container container-home">
-        <img src={image} alt="" className="main-image" />
+      <div className='container container-home'>
+        <img src={image} alt='' className='main-image' />
         <h1>Traditional and Digital Artist</h1>
-        <img src={logo} alt="" className="logo" />
+        <img src={logo} alt='' className='logo' />
       </div>
     </main>
   )
