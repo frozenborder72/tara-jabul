6c0fadfedc1aa4e5d783f685f63ecb1422043468
Marco <mar.manunta@gmail.com>
2022-08-13 10:41:54 +0200

    fixed home page pic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import logo from '../assets/images/LOGO.png'
-import image from '../assets/images/Digital Paintings/optimized/BICYCLE-GIRL.jpg'
+import image from '../assets/images/Traditional/optimized/Shaman.jpg'
 
 const Home = () => {
   return (
